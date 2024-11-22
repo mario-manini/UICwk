99b5e41d532ef84ebee922e70d2c576cc8748ec0
Simon Evans <146708387+Drew-Evs@users.noreply.github.com>
2024-11-22 15:34:48 +0000

    Complete backend implementation (ish)

---
+++ build/CMakeFiles/testRead.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/UICwk/determinand.cpp" "/workspaces/UICwk/build/CMakeFiles/testRead.dir/determinand.cpp.o"
+  "/workspaces/UICwk/sample.cpp" "/workspaces/UICwk/build/CMakeFiles/testRead.dir/sample.cpp.o"
+  "/workspaces/UICwk/sampleset.cpp" "/workspaces/UICwk/build/CMakeFiles/testRead.dir/sampleset.cpp.o"
+  "/workspaces/UICwk/testRead.cpp" "/workspaces/UICwk/build/CMakeFiles/testRead.dir/testRead.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/testRead.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testRead.dir/determinand.cpp.o"
+  "CMakeFiles/testRead.dir/sample.cpp.o"
+  "CMakeFiles/testRead.dir/sampleset.cpp.o"
+  "CMakeFiles/testRead.dir/testRead.cpp.o"
+  "testRead"
+  "testRead.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testRead.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
